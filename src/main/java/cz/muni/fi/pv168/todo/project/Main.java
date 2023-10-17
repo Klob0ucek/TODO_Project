903abee223cf0f26333ea5118bae3f07e9787eaf
Jan Polacek <janik.polacek@gmail.com>
2023-10-17 20:21:42 +0200

    GUI: Added invokeLater

--- src/main/java/cz/muni/fi/pv168/todo/project/Main.java
+++ src/main/java/cz/muni/fi/pv168/todo/project/Main.java
@@ -3,16 +3,15 @@ package cz.muni.fi.pv168.todo.project;
 import com.formdev.flatlaf.FlatDarkLaf;
 import cz.muni.fi.pv168.todo.project.ui.MainWindow;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        /* Setup for FlatLookAndFeel:
-         *  - FlatLightLaf
-         *  - FlatDarkLaf
-         *  - FlatDarculaLaf
-         */
-        FlatDarkLaf.setup();
-
-        MainWindow mainWindow = new MainWindow();
-        mainWindow.show();
+        Runnable flatLaf = () -> {
+            FlatDarkLaf.setup();
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.show();
+        };
+        SwingUtilities.invokeLater(flatLaf);
     }
 }
