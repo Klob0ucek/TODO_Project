6f1c0467e76e060d029911fe80abc08cc73c563a
Radim Rychlík <xrychli1@fi.muni.cz>
2023-10-17 18:51:31 +0200

    GUI: Date and time picker

--- src/main/java/cz/muni/fi/pv168/todo/project/ui/dialog/AddDialog.java
+++ src/main/java/cz/muni/fi/pv168/todo/project/ui/dialog/AddDialog.java
@@ -1,18 +1,22 @@
 package cz.muni.fi.pv168.todo.project.ui.dialog;
 
+import com.github.lgooddatepicker.components.DateTimePicker;
 import cz.muni.fi.pv168.todo.project.model.Category;
 import cz.muni.fi.pv168.todo.project.model.Event;
 import cz.muni.fi.pv168.todo.project.ui.model.ComboBoxModelAdapter;
 
-import javax.swing.*;
-import java.time.LocalDate;
-import java.time.LocalTime;
+import javax.swing.JTextField;
+import javax.swing.ComboBoxModel;
+import javax.swing.ListModel;
+import javax.swing.JComboBox;
+import java.time.Duration;
 import java.util.List;
 
 public class AddDialog extends EntityDialog<Event> {
     private final JTextField nameField = new JTextField();
     private final ComboBoxModel<Category> categoryModel;
     private final JTextField locationField = new JTextField();
+    private final DateTimePicker dateTimePicker = new DateTimePicker();
 
     private final Event event;
 
@@ -26,6 +30,7 @@ public class AddDialog extends EntityDialog<Event> {
         add("Name:", nameField);
         add("Category:", new JComboBox<>(categoryModel));
         add("Location:", locationField);
+        add("Date and time:", dateTimePicker);
     }
 
     @Override
@@ -34,8 +39,9 @@ public class AddDialog extends EntityDialog<Event> {
         event.setName(nameField.getText());
         event.setCategories(List.of((Category) categoryModel.getSelectedItem()));
         event.setLocation(locationField.getText());
-        event.setDate(LocalDate.of(2023, 10, 10));
-        event.setTime(LocalTime.of(10, 0));
+        event.setDate(dateTimePicker.getDatePicker().getDate());
+        event.setTime(dateTimePicker.getTimePicker().getTime());
+        event.setDuration(Duration.ofMinutes(30));
         return event;
     }
 }
