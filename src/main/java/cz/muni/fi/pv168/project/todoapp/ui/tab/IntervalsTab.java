1febbf59b71ee94c07fe8baea6f8f13cb499a63d
Jakub Nezval <xnezval@fi.muni.cz>
2023-10-18 18:27:41 +0200

    GUI: Fix intervals tab

--- src/main/java/cz/muni/fi/pv168/project/todoapp/ui/tab/IntervalsTab.java
+++ src/main/java/cz/muni/fi/pv168/project/todoapp/ui/tab/IntervalsTab.java
@@ -1,11 +1,20 @@
 package cz.muni.fi.pv168.project.todoapp.ui.tab;
 
 import cz.muni.fi.pv168.project.todoapp.ui.ToolBarManager;
+import cz.muni.fi.pv168.project.todoapp.ui.action.interval.AddInterval;
+import cz.muni.fi.pv168.project.todoapp.ui.action.interval.DeleteInterval;
+import cz.muni.fi.pv168.project.todoapp.ui.action.interval.EditInterval;
+
+import javax.swing.Action;
 import javax.swing.Icon;
 
 import java.awt.Component;
 
 public class IntervalsTab extends GeneralTab {
+    private final Action add = new AddInterval();
+    private final Action edit = new EditInterval();
+    private final Action delete = new DeleteInterval();
+
     public IntervalsTab(
             String title,
             Icon icon,
@@ -20,6 +29,9 @@ public class IntervalsTab extends GeneralTab {
     public void updateToolBar() {
         this.getToolBarManager()
                 .reset()
+                .addAction(ToolBarManager.ModifyAction.ADD, add)
+                .addAction(ToolBarManager.ModifyAction.EDIT, edit)
+                .addAction(ToolBarManager.ModifyAction.DELETE, delete)
                 .saveChanges();
     }
 }
