82d868a3ea14f03543c7297805af0d6b30e215f5
Jakub Nezval <xnezval@fi.muni.cz>
2023-10-27 16:19:17 +0200

    GUI: unification of filter selection

--- src/main/java/cz/muni/fi/pv168/project/todoapp/ui/filter/Filter.java
+++ src/main/java/cz/muni/fi/pv168/project/todoapp/ui/filter/Filter.java
@@ -4,11 +4,10 @@ import com.github.lgooddatepicker.components.DatePicker;
 
 import cz.muni.fi.pv168.project.todoapp.ui.filter.auxiliary.CheckboxGroup;
 
+import cz.muni.fi.pv168.project.todoapp.ui.util.EnumUtils;
 import javax.swing.ButtonGroup;
-import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JCheckBoxMenuItem;
-import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -21,13 +20,18 @@ import java.awt.GridLayout;
 import java.util.List;
 
 public class Filter {
-    private final JComboBox<IsDoneState> isDone = new JComboBox<>();
+    private final JMenuBar isDone = new JMenuBar();
+    private final ButtonGroup isDoneOptions = new ButtonGroup();
+
     private final JMenuBar categories = new JMenuBar();
     private final CheckboxGroup categoryOptions = new CheckboxGroup();
+
     private final DatePicker fromDate = new DatePicker();
     private final DatePicker toDate = new DatePicker();
+
     private final JMenuBar intervals = new JMenuBar();
     private final ButtonGroup intervalOptions = new ButtonGroup();
+
     private final JButton resetButton = new JButton("Reset");
 
     private enum IsDoneState {
@@ -37,29 +41,17 @@ public class Filter {
     public Filter(
             /* should have access to all currently existing Categories and Intervals */
     ) {
+        initIsDone();
         initCategories();
         initIntervals();
 
-        isDone.setModel(new DefaultComboBoxModel<>(IsDoneState.values()));
-        // isDone.setRenderer();
-
         resetButton.addActionListener(e -> {
-            isDone.setSelectedIndex(0);
+            isDoneOptions.clearSelection();
             categoryOptions.clearSelection();
-            intervalOptions.clearSelection();
             fromDate.clear();
             toDate.clear();
+            intervalOptions.clearSelection();
         });
-    }
-
-    private static JComponent createNamedComponent(
-            String name,
-            JComponent component
-    ) {
-        var named = new JPanel();
-        named.add(new JLabel(name));
-        named.add(component);
-        return named;
     }
 
     private static JComponent createNamedVerticalPair(
@@ -83,38 +75,55 @@ public class Filter {
         return blob;
     }
 
+    private void initIsDone() {
+        var options = new JMenu("Done?");
+        JRadioButtonMenuItem option;
+
+        for (var optEnum : IsDoneState.values()) {
+            option = new JRadioButtonMenuItem(EnumUtils.toTitle(optEnum));
+            options.add(option);
+            isDoneOptions.add(option);
+        }
+
+        isDone.add(options);
+    }
+
     private void initCategories() {
-        var menu = new JMenu("Category");
+        var options = new JMenu("Category");
         JCheckBoxMenuItem option;
 
-        for (var x : List.of("a", "b", "c", "d", "e", "Very long string, I'm curious what's gonna happen.")) {
+        for (var x : List.of(
+                "Irrelevant", "a", "b", "c", "d", "e", "Very long string, I'm curious what's gonna happen."
+        )) {
             /*       ^^ list of available categories ^^ */
             option = new JCheckBoxMenuItem(x);
-            menu.add(option);
+            options.add(option);
             categoryOptions.add(option);
         }
 
-        categories.add(menu);
+        categories.add(options);
     }
 
     private void initIntervals() {
-        var menu = new JMenu("Interval");
+        var options = new JMenu("Interval");
         JRadioButtonMenuItem option;
 
-        for (var x : List.of("a", "b", "c", "d", "e", "Very long string, I'm curious what's gonna happen.")) {
+        for (var x : List.of(
+                "Irrelevant", "a", "b", "c", "d", "e", "Very long string, I'm curious what's gonna happen."
+        )) {
             /*       ^^ list of available intervals ^^ */
             option = new JRadioButtonMenuItem(x);
-            menu.add(option);
+            options.add(option);
             intervalOptions.add(option);
         }
 
-        intervals.add(menu);
+        intervals.add(options);
     }
 
     public JComponent getFilterBar() {
         var filterBar = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
-        filterBar.add(createNamedComponent("Done?:", isDone));
+        filterBar.add(isDone);
         filterBar.add(categories);
         filterBar.add(createNamedVerticalPair("From:", fromDate, "To:", toDate));
         filterBar.add(intervals);
