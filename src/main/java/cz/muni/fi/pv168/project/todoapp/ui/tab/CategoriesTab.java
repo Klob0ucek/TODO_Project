4a19120c062be715be39458131f593852c136dc9
Jan Polacek <janik.polacek@gmail.com>
2023-11-09 00:36:40 +0100

    GUI: removed * import

--- src/main/java/cz/muni/fi/pv168/project/todoapp/ui/tab/CategoriesTab.java
+++ src/main/java/cz/muni/fi/pv168/project/todoapp/ui/tab/CategoriesTab.java
@@ -4,7 +4,7 @@ import cz.muni.fi.pv168.project.todoapp.ui.action.category.AddCategory;
 import cz.muni.fi.pv168.project.todoapp.ui.action.category.DeleteCategory;
 import cz.muni.fi.pv168.project.todoapp.ui.action.category.EditCategory;
 
-import javax.swing.*;
+import javax.swing.JTable;
 
 public class CategoriesTab extends GeneralTab {
     public static class BuildTemplate extends GeneralTab.BuildTemplate<BuildTemplate> {
