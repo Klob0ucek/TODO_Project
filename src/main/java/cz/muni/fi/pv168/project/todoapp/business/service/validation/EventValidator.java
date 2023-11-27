959e5cf7184e2373bdcbab8d326ac33b4ffc97ed
Jan Polacek <janik.polacek@gmail.com>
2023-11-27 21:14:47 +0100

    Updated Validators to validate data only if mandatory or present

--- src/main/java/cz/muni/fi/pv168/project/todoapp/business/service/validation/EventValidator.java
+++ src/main/java/cz/muni/fi/pv168/project/todoapp/business/service/validation/EventValidator.java
@@ -1,11 +1,12 @@
 package cz.muni.fi.pv168.project.todoapp.business.service.validation;
 
-import cz.muni.fi.pv168.project.todoapp.business.model.Category;
 import cz.muni.fi.pv168.project.todoapp.business.model.Event;
 import cz.muni.fi.pv168.project.todoapp.business.service.validation.common.CategoryListValidator;
 import cz.muni.fi.pv168.project.todoapp.business.service.validation.common.GuidValidator;
 import cz.muni.fi.pv168.project.todoapp.business.service.validation.common.StringLengthValidator;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import static cz.muni.fi.pv168.project.todoapp.business.service.validation.Validator.extracting;
 
@@ -13,12 +14,16 @@ public class EventValidator implements Validator<Event> {
 
     @Override
     public ValidationResult validate(Event model) {
-        var validators = List.of(
+        var validators = new ArrayList<>(List.of(
                 extracting(Event::getName, new StringLengthValidator(2, 60, "Name")),
-                extracting(Event::getLocation, new StringLengthValidator(2, 150, "Location")),
                 extracting(Event::getCategories, new CategoryListValidator("Category List")),
                 extracting(Event::getGuid, new GuidValidator("Guid"))
-        );
+        ));
+
+        if (model.getLocation() != null && !model.getLocation().isEmpty()) {
+            System.out.println("Empty location");
+            validators.add(extracting(Event::getLocation, new StringLengthValidator(2, 150, "Location")));
+        }
 
         return Validator.compose(validators).validate(model);
     }
--- src/main/java/cz/muni/fi/pv168/project/todoapp/business/service/validation/TemplateValidator.java
+++ src/main/java/cz/muni/fi/pv168/project/todoapp/business/service/validation/TemplateValidator.java
@@ -7,19 +7,27 @@ import cz.muni.fi.pv168.project.todoapp.business.model.Template;
 import cz.muni.fi.pv168.project.todoapp.business.service.validation.common.CategoryListValidator;
 import cz.muni.fi.pv168.project.todoapp.business.service.validation.common.GuidValidator;
 import cz.muni.fi.pv168.project.todoapp.business.service.validation.common.StringLengthValidator;
+import java.util.ArrayList;
 import java.util.List;
 import static cz.muni.fi.pv168.project.todoapp.business.service.validation.Validator.extracting;
 
 public class TemplateValidator implements Validator<Template> {
     @Override
     public ValidationResult validate(Template model) {
-        var validators = List.of(
+        var validators = new ArrayList<>(List.of(
                 extracting(Template::getTemplateName, new StringLengthValidator(2, 60, "Template name")),
                 extracting(Template::getName, new StringLengthValidator(2, 60, "Event Name")),
-                extracting(Template::getLocation, new StringLengthValidator(2, 150, "Location")),
                 extracting(Template::getCategories, new CategoryListValidator("Category List")),
                 extracting(Template::getGuid, new GuidValidator("Guid"))
-        );
+        ));
+
+        if (model.getName() != null && !model.getName().isEmpty()) {
+            validators.add(extracting(Template::getName, new StringLengthValidator(2, 60, "Event Name")));
+        }
+
+        if (model.getLocation() != null && !model.getLocation().isEmpty()) {
+            validators.add(extracting(Template::getLocation, new StringLengthValidator(2, 150, "Location")));
+        }
 
         return Validator.compose(validators).validate(model);
     }
