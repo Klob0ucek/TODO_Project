e60921c4b77e9fd71d8be98a04e79a558fec018f
Jan Polacek <janik.polacek@gmail.com>
2023-12-02 21:49:32 +0100

    GUI: Added simple statistic popup panel

--- src/main/java/cz/muni/fi/pv168/project/todoapp/ui/resources/Icons.java
+++ src/main/java/cz/muni/fi/pv168/project/todoapp/ui/resources/Icons.java
@@ -13,7 +13,10 @@ public enum Icons {
     EDIT("edit-pen-big-icon.png"),
     EXPORT("file-download-big-icon.png"),
     IMPORT("file-upload-big-icon.png"),
-    EXIT("exit-left-big-icon.png");
+    EXIT("exit-left-big-icon.png"),
+
+    UP("statistics-up-icon.png"),
+    DOWN("statistics-down-icon.png");
 
     private final String iconPath;
 
@@ -21,15 +24,24 @@ public enum Icons {
         this.iconPath = iconPath;
     }
 
+    public Icon getSmallIcon() {
+        Image newImage = setUpImage().getScaledInstance(24, 24, Image.SCALE_AREA_AVERAGING);
+        return new ImageIcon(newImage);
+    }
+
     public Icon getIcon() {
+        Image newImage = setUpImage().getScaledInstance(34, 34, Image.SCALE_AREA_AVERAGING);
+        ;
+        return new ImageIcon(newImage);
+    }
+
+    public Image setUpImage() {
         try {
             URL url = Icons.class.getResource(iconPath);
             if (url == null) {
                 throw new IllegalArgumentException("Icon resource not found on classpath: " + iconPath);
             }
-            Image originalImage = ImageIO.read(url);
-            Image scaledImage = originalImage.getScaledInstance(34, 34, Image.SCALE_AREA_AVERAGING);
-            return new ImageIcon(scaledImage);
+            return ImageIO.read(url);
         } catch (IOException e) {
             throw new IllegalArgumentException("Icon resource not found on classpath: " + iconPath);
         }
--- src/main/java/cz/muni/fi/pv168/project/todoapp/ui/statistics/Statistics.java
+++ src/main/java/cz/muni/fi/pv168/project/todoapp/ui/statistics/Statistics.java
@@ -1,33 +1,77 @@
 package cz.muni.fi.pv168.project.todoapp.ui.statistics;
 
 import cz.muni.fi.pv168.project.todoapp.business.service.crud.CrudHolder;
+import cz.muni.fi.pv168.project.todoapp.ui.resources.Icons;
+import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JButton;
 
 public class Statistics {
     private final CrudHolder crudHolder;
+    private final JPanel statsPanel;
+    private final JButton toggleButton;
 
     public Statistics(CrudHolder crudHolder) {
         this.crudHolder = crudHolder;
+        this.statsPanel = createStatsPanel();
+        this.toggleButton = createToggleButton();
     }
 
-    public Component getStats() {
+    public JPanel createStatsPanel() {
         JPanel panel = new JPanel();
-        new BoxLayout(panel, BoxLayout.PAGE_AXIS);
+        new BoxLayout(panel, BoxLayout.X_AXIS);
+        fillData(panel);
+        panel.setVisible(false);
+        return panel;
+    }
+
+    private void fillData(JPanel panel) {
         panel.add(getLabel("Planned events: ", crudHolder.getPlannedEventsCount()));
         panel.add(getLabel("Finished events: ", crudHolder.getDoneEventsCount()));
         panel.add(getLabel("Events total: ", crudHolder.getEvents().size()));
         panel.add(getLabel("Categories: ", crudHolder.getCategories().size()));
         panel.add(getLabel("Templates: ", crudHolder.getTemplates().size()));
         panel.add(getLabel("Intervals: ", crudHolder.getIntervals().size()));
+    }
 
-        return panel;
+    private void refreshData() {
+        statsPanel.removeAll();
+        fillData(statsPanel);
     }
 
     private JLabel getLabel(String name, long count) {
-        return new JLabel(name + count);
+        return new JLabel("  " + name + count + "  ");
     }
 
+    private JButton createToggleButton() {
+        JButton button = new JButton(Icons.UP.getSmallIcon());
+        button.setToolTipText("Show statistics");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!statsPanel.isVisible()) {
+                    refreshData();
+                    toggleButton.setIcon(Icons.DOWN.getSmallIcon());
+                    toggleButton.setToolTipText("Hide statistics + refresh");
+                } else {
+                    toggleButton.setToolTipText("Show statistics");
+                    toggleButton.setIcon(Icons.UP.getSmallIcon());
+                }
+                statsPanel.setVisible(!statsPanel.isVisible());
+            }
+        });
+        return button;
+    }
+
+    public Component getStats() {
+        JPanel containerPanel = new JPanel(new BorderLayout());
+        containerPanel.add(toggleButton, BorderLayout.NORTH);
+        containerPanel.add(statsPanel, BorderLayout.CENTER);
+        return containerPanel;
+    }
 }
